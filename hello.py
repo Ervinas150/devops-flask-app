7af9898bb1932aaf7f793b1dda80d9f0927c7d74
Ervinas Naujokas <C24375021@mytudublin.ie>
2026-09-25 12:30:10 +0100

    contact page added

--- hello.py
+++ hello.py
@@ -5,3 +5,9 @@ app=Flask(__name__)
 @app.route('/')
 def say_hello():
     return '<p>Hello, World, I am a Flask app!</p>'
+
+@app.route('/about')
+def say_about():
+    return 'My name is Ervinas <br> My number is 12345678'
+
+
